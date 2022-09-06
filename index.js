40c13bd4dfd66d5cc94544fb2e7f2dcab86abf18
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-06 22:54:34 +0200

    created orders route file

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const { PORT } = process.env
-const productRoutes = require("./routes/product")
+const productRoutes = require("./routes/products")
 const categoryRoutes = require("./routes/category")
+const ordersRoutes = require("./routes/orders")
 
 require("./models/index")
 
 app.use("/products", productRoutes)
 app.use("/category", categoryRoutes)
+app.use("/orders", ordersRoutes)
 
 app.listen(PORT, (req, res) => {
   console.log(`Running on ${PORT}`)
