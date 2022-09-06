2e6a3d2061772b922f3a8437c9434dd3b8cda9b1
Walid <belkassmi.walid@gmail.com>
2022-09-06 21:25:30 +0200

    all products of a category

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const { Category } = require("../models")
+const { Product } = require("../models")
 
 // Get all categories
 app.get("/", async (req, res) => {
@@ -34,6 +35,23 @@ app.get("/:id", async (req, res) => {
 })
 
 // Get all product of a category
-// app.get("/:id/product")
+app.get("/:id/products", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const products = await Product.findAll({
+      where: {
+        id,
+      },
+    })
+    if (products) {
+      req.status(201).json(products)
+    } else {
+      res.status(404).json("Products not found")
+    }
+  } catch (e) {
+    res.status(500).json("Internal server error")
+  }
+})
 
 module.exports = app
