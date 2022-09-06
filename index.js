dd8fa53863f608bf7c0c24586aa3d89c8bbb7174
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-06 14:59:49 +0200

    base

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app =  express()
-const port = 5000
 
-app.listen(port, (req, res) => {
-  console.log(`Running on ${port}`)
+const { DB_PORT } = process.env
+
+app.listen(DB_PORT, (req, res) => {
+  console.log(`Running on ${DB_PORT}`)
 })
