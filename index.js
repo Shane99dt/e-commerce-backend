5dc79a6065fc515fc3c6b4191493aeb01a6749e1
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-06 15:58:31 +0200

    base

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app =  express()
-const { DB_PORT } = process.env
+const { PORT } = process.env
 const productRoutes = require('./routes/product')
 
 require('./models/index')
 
 app.use('/products', productRoutes)
 
-app.listen(DB_PORT, (req, res) => {
-  console.log(`Running on ${DB_PORT}`)
+app.listen(PORT, (req, res) => {
+  console.log(`Running on ${PORT}`)
 })
